a2d32b8db075de9475462689f42fa84fb84d577b
yd27 <dhdbseh5oe@naver.com>
2020-07-18 11:23:32 +0900

    포토뷰 라이브러리 활용

--- app/src/main/java/kr/co/tjoeun/library_20200718/BaseActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/BaseActivity.kt
@@ -1,6 +1,12 @@
 package kr.co.tjoeun.library_20200718
 
+import androidx.appcompat.app.AppCompatActivity
 
 
-abstract class BaseActivity {
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
 }
--- app/src/main/java/kr/co/tjoeun/library_20200718/ProfilePhotoActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/ProfilePhotoActivity.kt
@@ -1,28 +1,20 @@
 package kr.co.tjoeun.library_20200718
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+class ProfilePhotoActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupEvents()
-        setValues()
-
+        setContentView(R.layout.activity_profile_photo)
     }
 
     override fun setupEvents() {
-
-        val myIntent = Intent(mContext, ProfilePhotoActivity)
-        startActivity(myIntent)
 
     }
 
     override fun setValues() {
 
     }
-
 }
